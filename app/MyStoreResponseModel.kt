c82380a7abbaf299198d2f43ad9dabb8128d63d3
chanho <uni070@naver.com>
2023-12-22 17:40:05 +0900

    2023/12/22

---
+++ app/MyStoreResponseModel.kt
@@ -0,0 +1,4 @@
+data class MyStoreResponseModel(
+    val image: String,
+    val result: Result
+)
---
+++ app/Result.kt
@@ -0,0 +1,10 @@
+data class Result(
+    val CRN: String,
+    val address: String,
+    val ceoName: String,
+    val contact: String,
+    val kind: String,
+    val latitude: String,
+    val longitude: String,
+    val storename: String
+)
